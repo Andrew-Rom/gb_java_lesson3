59da9d966732f60c18967b4c2096b09a82394f9c
And-Rom <you@example.com>
2023-02-16 23:01:06 +0300

    Lesson 3 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+//        ArrayList<Integer> randomList = new ArrayList<>();
+//        Random f = new Random();
+//        for (int i = 0; i < 10; i++) {
+//            randomList.add(f.nextInt(101));
+//        }
+//        System.out.println(randomList.toString());
+//
+//        Collections.sort(randomList);
+//        System.out.println(randomList.toString());
+
+        /**
+         * Заполнить список любыми строкам в произвольном порядке с повторениями.
+         * Вывести название каждой планеты и количество его повторений в списке.
+         */
+
+//        ArrayList<String> planets = new ArrayList<>(List.of("Земля", "Земля", "Юпитер", "Марс",
+//                "Венера", "Плутон", "Земля", "Юпитер", "Марс", "Земля"));
+//        ArrayList<String> copyPlanets = new ArrayList<>();
+//        for (String planet:planets) {
+//            int counter = 0;
+//            if (copyPlanets.contains(planet)) {
+//                continue;
+//            } else {
+//
+//                for (int i = 0; i < planets.size(); i++) {
+//                    if (planet.equals(planets.get(i))) {
+//                        counter++;
+//                    }
+//                }
+//            }
+//            copyPlanets.add(planet);
+//            System.out.println(planet + ": " + counter);
+//        }
+//    }
+//
+//        for (int i = 0; i < planets.size(); i++) {
+//            int counter = 0;
+//            if (!(copyPlanets.contains(planets.get(i)))) {
+//                for (int j = 0; j < planets.size(); j++) {
+//                    if (planets.get(i).equals(planets.get(j))) {
+//                        counter++;
+//                    }
+//                }
+//            }
+//            copyPlanets.add(planets.get(i));
+//            System.out.println(planets.get(i) + ": " + counter);
+//        }
     }
 }
